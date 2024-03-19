015ff120acf04fbad539623df011264079d9b0a8
gypgyptestmore <2908400267@qq.com>
2024-03-19 20:25:25 +0800

    feat:组合式api

--- vue3_ts/src/App.vue
+++ vue3_ts/src/App.vue
@@ -1,30 +1,22 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app">
+    <h1>你好👋</h1>
+    <person></person>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+<script lang="ts">
+import Person from "./components/Person.vue";
+export default {
+  name: "App",
+  components: {
+    Person,
+  },
+};
+</script>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.app {
+  background-color: rgb(115, 187, 201);
 }
 </style>
---
+++ vue3_ts/src/components/Person.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="person">
+    {{ date }}
+    {{ name }}
+    {{ age }}
+    <button @click="changeName">点击改名字</button>
+    <button @click="changeDate">点击改日期</button>
+    <button @click="changeNum">点击改数子</button>
+  </div>
+</template>
+  
+  <script lang="ts">
+export default {
+  name: "Person",
+  data() {
+    return {
+      date: "周二",
+      name: "张三",
+      age: 1,
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = "我改名字了";
+    },
+    changeDate() {
+      this.date = "周六去植物园逛逛";
+    },
+    changeNum() {
+      this.age += 1;
+    },
+  },
+};
+</script>
+  
+  <style scoped>
+.person {
+  background-color: pink;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+button {
+  margin: 0 15px;
+}
+</style>
